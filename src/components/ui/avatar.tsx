319f5c3abefe3cc6d3fb95fb69692a8e3ae02aa1
CalebBlion <kaleba647@gmail.com>
2023-07-18 22:32:40 +0300

    Slightly modified the style

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full border bg-muted",
+      "flex h-full font-medium w-full items-center justify-center rounded-full border bg-muted",
       className
     )}
     {...props}
