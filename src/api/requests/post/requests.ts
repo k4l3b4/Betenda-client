c21bdcceb9c52713c3963ff9c23a14870d9725ee
CalebBlion <kaleba647@gmail.com>
2023-07-18 22:28:30 +0300

    ADD: api route fn:- getposts, crtpost, updtpost, dltpost

---
+++ src/api/requests/post/requests.ts
@@ -0,0 +1,52 @@
+import axiosInstance from "@/api/axios-instance"
+import { uploadProgress } from "@/lib/utils"
+import { CreatePostType } from "@/types/post"
+import FormData from "form-data"
+
+export const getPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+    const response = await axiosInstance.get(`posts/get_posts?page=${pageParam}`)
+    return response.data
+}
+
+export const createPost = async ({ values, parent }: { values: CreatePostType, parent?: number | undefined }) => {
+    const data = new FormData()
+    { values?.content ? data.append('content', values?.content) : undefined }
+    { values?.media ? data.append('media', values?.media, values?.media?.name) : undefined }
+
+    const response = await axiosInstance.post(`posts/post${parent ? `?parent_id=${parent}` : ''}`, data, {
+        onUploadProgress: (progressEvent) => {
+            const percentCompleted = Math.round(
+              (progressEvent?.loaded * 100) / progressEvent?.total
+            );
+            uploadProgress['create-post'] = percentCompleted; // Update the upload progress for the given key
+          },
+        timeout: 90000,
+        headers: {
+            'Content-Type': values.media?.type
+        }
+    })
+
+    return response.data
+}
+
+
+export const updatePost = async ({ values, id }: { values: CreatePostType, id: number }) => {
+    const data = new FormData()
+
+    data.append('id', id)
+    { values?.content ? data.append('content', values?.content) : undefined }
+    { values?.media ? data.append('media', values?.media, values?.media?.name) : undefined }
+
+    const response = await axiosInstance.patch("posts/post", data, {
+        timeout: 90000,
+        headers: {
+            'Content-Type': values.media?.type
+        }
+    })
+    return response.data
+}
+
+export const deletePost = async ({ id }: { id: number }) => {
+    const response = await axiosInstance.delete(`posts/post?id=${id}`)
+    return response.data
+}
