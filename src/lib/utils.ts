53dd440d8782d7ad5c3849664602ec719b29d791
CalebBlion <kaleba647@gmail.com>
2023-07-18 22:36:42 +0300

    add guessMedia helper to detect file type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,34 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+interface UploadProgress {
+  [key: string]: number;
+}
+
+export const uploadProgress: UploadProgress = {};
+
+
+
+export function guessMedia(link: string): string | boolean {
+  const extension = link.match(/\.([^.]+)$/)?.[1]?.toLowerCase();
+
+  if (extension) {
+    if (['jpg', 'jpeg', 'png', 'gif'].includes(extension)) {
+      return "img";
+    }
+
+    if (['mp4', 'webm', 'ogg'].includes(extension)) {
+      return "vid";
+    }
+
+    if (['mp3', 'wav', 'ogg'].includes(extension)) {
+      return "audio";
+    }
+  }
+
+  return false;
 }
