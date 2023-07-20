528c25a3a2350539b68dfeb08e3f228bccb8f287
CalebBlion <kaleba647@gmail.com>
2023-07-20 23:48:11 +0300

    Avatar Rounded-md

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-md",
       className
     )}
     {...props}
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full font-medium w-full items-center justify-center rounded-full border bg-muted",
+      "flex h-full font-medium w-full items-center justify-center rounded-md border bg-muted",
       className
     )}
     {...props}
