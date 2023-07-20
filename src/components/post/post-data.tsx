9bdd27a7c08a80ea027502c6c1e604ca9162ff5f
CalebBlion <kaleba647@gmail.com>
2023-07-20 23:44:41 +0300

    Abstracted the profile hover card custom link match implementation

--- src/components/post/post-data.tsx
+++ src/components/post/post-data.tsx
@@ -3,32 +3,26 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Heart, Share2, MessageCircle, CalendarDays, Star } from "lucide-react"
 import { PostType } from "@/types/post";
 import Image from "next/image";
-import { DateAndMonth, RelativeTime } from "@/components/time/Time";
-import Linkify from "linkify-react";
+import { RelativeTime } from "@/components/time/Time";
 import Link from "next/link";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "../ui/hover-card";
 import { guessMedia } from "@/lib/utils";
+import { DialogContent, DialogTrigger } from "../ui/dialog";
+import { Dialog } from "@radix-ui/react-dialog";
+import CreatePost from "./create-post";
+import convertTextToLinks from "../convert-text-to-links/convert-text-to-links";
+import ReplyPost from "./reply-post";
+import ProfileHoverCard from "../profile-hover-card/profile-hover-card";
 
-const renderLink = ({ attributes, content }: { attributes: any, content: string }) => {
-    const { href, ...props } = attributes;
-    return <Link href={href} {...props}>{content}</Link>;
-};
-
-const link_options = {
-    formatHref: {
-        mention: (href: string) => "http://localhost:3000/" + href,
-        hashtag: (href: string) => "http://localhost:3000/tag/" + href.substr(1),
-    },
-    render: {
-        hashtag: renderLink,
-        mention: renderLink,
-    },
-}
 
 const PostData = ({ post }: { post: PostType }) => {
     return (
-        <div className="w-[550px] p-2">
-            <div>
+        <div className="relative w-full max-w-[650px] rounded-md p-4 my-2 bg-foreground">
+            <div className="relative">
+                {post?.thread?.length > 0 ?
+                    <span className="rounded-full absolute w-[3px] h-full left-5 top-12 bg-gray-400/50" />
+                    :
+                    null
+                }
                 <div className="flex items-center gap-x-2" id="user">
                     <Avatar>
                         {post?.user?.profile_avatar ?
@@ -40,60 +34,20 @@ const PostData = ({ post }: { post: PostType }) => {
                     </Avatar>
                     <div className="flex items-baseline gap-x-2">
                         <div>
-                            <HoverCard>
-                                <HoverCardTrigger className="no-underline">
-                                    <Link href={`/${post?.user?.user_name}`} className="font-medium no-underline">{`${post?.user?.first_name} ${post?.user?.last_name}`}</Link>
-                                </HoverCardTrigger>
-                                <HoverCardContent className="w-80">
-                                    <div className="flex justify-between space-x-4">
-                                        <Avatar>
-                                            {post?.user?.profile_avatar ?
-                                                <AvatarImage src={post?.user?.profile_avatar} alt={post?.user?.user_name} />
-                                                :
-                                                null
-                                            }
-                                            <AvatarFallback>{`${post?.user?.first_name?.substr(0, 1)}${post?.user?.last_name?.substr(0, 1)}`}</AvatarFallback>
-                                        </Avatar>
-                                        <div className="space-y-1">
-                                            <div>
-                                                <h4 className="text-sm font-semibold">{`${post?.user?.first_name} ${post?.user?.last_name}`}</h4>
-                                                <p className="text-xs text-muted-foreground">{`@${post?.user?.user_name}`}</p>
-                                            </div>
-                                            <p className="text-sm">
-                                                {post?.user?.bio ?? "No bio, Please nag this user into creating a bio for his account."}
-                                            </p>
-                                            <div>
-                                                <div className="flex items-center pt-2">
-                                                    <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-                                                    <span className="text-xs text-muted-foreground">
-                                                        Joined {DateAndMonth(post?.user?.joined_date)}
-                                                    </span>
-                                                </div>
-                                                <div className="flex items-center pt-2">
-                                                    <Star className="mr-2 h-4 w-4 opacity-70" />{" "}
-                                                    <span className="text-xs text-muted-foreground">
-                                                        The {post?.user?.id} user to join betenda
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </HoverCardContent>
-                            </HoverCard>
+                            <ProfileHoverCard user={post?.user}>
+                                <Link href={`/${post?.user?.user_name}`} className="font-medium no-underline">{`${post?.user?.first_name} ${post?.user?.last_name}`}</Link>
+                            </ProfileHoverCard>
                             <p className="text-xs text-muted-foreground">{`@${post?.user?.user_name}`}</p>
                         </div>
                         <span className="text-xs">•</span>
                         <p className="text-xs text-muted-foreground">{RelativeTime(post?.created_at)}</p>
                     </div>
                 </div>
-                <div className="p-2 text-sm" id="post">
-                    <Linkify as="p" className="text-base mb-1"
-                        options={link_options}>
-                        {post?.content}
-                    </Linkify>
+                <div className="ml-10 max-w-full p-2 text-sm" id="post">
+                    <p className="text-base mb-1 link">{convertTextToLinks(post?.content as string)}</p>
                     {post?.media ?
                         guessMedia(post?.media) === "img" ?
-                            <Image className="max-w-full object-cover h-auto rounded-md" src={post?.media} width="500" height="800" alt={post?.content ?? ""} />
+                            <Image className="w-full max-w-full object-cover h-auto rounded-md" src={post?.media} width="500" height="800" alt={post?.content ?? ""} />
                             :
                             guessMedia(post?.media) === "vid" ?
                                 <video controls className="w-full max-w-full object-cover h-auto rounded-md" src={post?.media}></video>
@@ -104,24 +58,50 @@ const PostData = ({ post }: { post: PostType }) => {
                     }
                 </div>
             </div>
-            <section id="actions" className="flex flex-row justify-around">
+            <section id="actions" className="w-full flex flex-row justify-around">
                 <div className="flex flex-row items-center text-center">
-                    <Button variant={null} size="icon">
+                    <Button className="space-x-1 w-fit" variant={null} size="icon">
                         <Heart className="h-5 w-5" />
+                        <p className="text-xs font-medium">{post?.reactions?.reaction_count[0]?.count ?? "9.5K"}</p>
                     </Button>
-                    <p className="text-xs font-medium">{post?.reactions?.reaction_count[0]?.count ?? 0}</p>
                 </div>
                 <div className="flex flex-row items-center text-center">
-                    <Button variant={null} size="icon">
-                        <MessageCircle className="h-5 w-5" />
-                    </Button>
-                    <p className="text-xs font-medium">13K</p>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button className="space-x-1 w-fit" variant={null} size="icon">
+                                <MessageCircle className="h-5 w-5" />
+                                <p className="text-xs font-medium">{post?.reactions?.reaction_count[0]?.count ?? "2.9K"}</p>
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent className="w-full max-w-[700px] bg-foreground">
+                            <CreatePost placeholder="Reply to your hearts content" parent_id={post?.id} />
+                        </DialogContent>
+                    </Dialog>
                 </div>
                 <div>
                     <Button variant={null} size="icon">
                         <Share2 className="h-5 w-5" />
                     </Button>
                 </div>
+            </section>
+            <section id="thread">
+                {post?.thread?.length > 0 ?
+                    post?.thread?.slice(0, 2)?.map((post, index) => {
+                        const showLine = index === 0 && post?.thread?.length > 1;
+                        return (
+                            <section className="relative" key={post?.id}>
+                                {showLine ?
+                                    <span className="rounded-full absolute w-[3px] h-[80%] left-5 top-16 bg-gray-400/50" />
+                                    :
+                                    null
+                                }
+                                <ReplyPost post={post} parent_id={post?.id} />
+                            </section>
+                        )
+                    })
+                    :
+                    null
+                }
             </section>
         </div>
     );
