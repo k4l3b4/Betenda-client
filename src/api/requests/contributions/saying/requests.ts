90391caccbde7ae1884fe6a9b2b6925925f71061
CalebBlion <kaleba647@gmail.com>
2023-07-20 23:30:54 +0300

    ADD: saying create and update request functions

---
+++ src/api/requests/contributions/saying/requests.ts
@@ -0,0 +1,14 @@
+import axiosInstance from "@/api/axios-instance"
+import { CreateSayingType } from "@/types/contributions"
+
+
+export const registerSaying = async (values: CreateSayingType) => {
+    const response = await axiosInstance.post('contributions/saying', values)
+    return response?.data
+}
+
+
+export const updateSaying = async ({ values, id }: { values: CreateSayingType, id: number }) => {
+    const response = await axiosInstance.post(`contributions/saying?id=${id}`, values)
+    return response?.data
+}
