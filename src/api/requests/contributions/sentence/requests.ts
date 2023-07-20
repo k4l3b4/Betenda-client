211d77bd14855af20b15c2589e70cf32699c5b75
CalebBlion <kaleba647@gmail.com>
2023-07-20 23:31:11 +0300

    ADD: sentence create and update request functions

---
+++ src/api/requests/contributions/sentence/requests.ts
@@ -0,0 +1,14 @@
+import axiosInstance from "@/api/axios-instance"
+import { CreateSentenceType } from "@/types/contributions"
+
+
+export const registerSentence = async (values: CreateSentenceType) => {
+    const response = await axiosInstance.post('contributions/sentence', values)
+    return response?.data
+}
+
+
+export const updateSentence = async ({ values, id }: { values: CreateSentenceType, id: number }) => {
+    const response = await axiosInstance.post(`contributions/sentence?id=${id}`, values)
+    return response?.data
+}
