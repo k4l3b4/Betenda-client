46aab065dfd73f7a91aa7b6d2c8a086d1a365aae
CalebBlion <kaleba647@gmail.com>
2023-07-21 23:13:04 +0300

    Updated styles

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,35 +1,27 @@
 import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Bell, Plus } from "lucide-react"
 import { useUserContext } from "@/context/user-context"
 
+const ngClass = "no-underline hover:text-primary"
+
 export function SiteHeader() {
   const { User } = useUserContext()
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 w-full border-b bg-foreground">
       <div className="container flex h-16 items-center justify-between space-x-4 sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex select-none items-center space-x-1">
-            <Button size="icon" variant="ghost">
+            <Link href="/contributions" className={buttonVariants({ variant: "ghost" })}>
               <Plus className="h-5 w-5" />
-            </Button>
+            </Link>
             <ThemeToggle />
             <Button size="icon" variant={null}>
               <Bell className="h-5 w-5" />
@@ -39,7 +31,7 @@ export function SiteHeader() {
                 <Button className="w-fit rounded-full" size="icon" variant={null}>
                   <Avatar>
                     <AvatarImage src={User?.profile_avatar} alt={`@${User?.user_name}`} />
-                    <AvatarFallback>{`${User?.first_name?.substr(0,1)}${User?.last_name?.substr(0,1)}`}</AvatarFallback>
+                    <AvatarFallback>{`${User?.first_name?.substr(0, 1)}${User?.last_name?.substr(0, 1)}`}</AvatarFallback>
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
@@ -50,20 +42,29 @@ export function SiteHeader() {
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    Profile
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Link className={ngClass} href="/profile">Profile</Link>
                     <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
                   </DropdownMenuItem>
 
-                  <DropdownMenuItem>
-                    Settings
+                  <DropdownMenuItem className="cursor-pointer">
+                    <Link className={ngClass} href="/profile/settings">
+                      Settings
+                    </Link>
                     <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
                   </DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>GitHub</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <Link className={ngClass} href="/profile/settings">
+                    GitHub
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <Link className={ngClass} href="/profile/settings">
+                    Support
+                  </Link>
+                </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>
                   Log out
