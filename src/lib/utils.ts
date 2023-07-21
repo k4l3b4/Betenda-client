ea180a91b87352b5423d5f6025e8d04108fe187b
CalebBlion <kaleba647@gmail.com>
2023-07-21 23:14:02 +0300

    Added a number formatter and an error renderer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import ApiError from "@/types/api";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -31,4 +32,43 @@ export function guessMedia(link: string): string | boolean {
   }
 
   return false;
+}
+
+
+
+export const formatNumberWithSuffix = (number: number) => {
+  const suffixes = ["", "k", "M", "B", "T", "Q"];
+
+  if (number < 1000) {
+    return number.toString();
+  }
+
+  const magnitude = Math.floor(Math.log10(number) / 3);
+  const scaledNumber = number / Math.pow(10, magnitude * 3);
+
+  return scaledNumber.toFixed(1).replace(/\.0$/, "") + suffixes[magnitude];
+}
+
+
+export const renderErrors = (error: ApiError): string | { [field: string]: string[] | string } => {
+  if (typeof error.error === "string") {
+    // If the error is a simple string, return it directly
+    return error?.error;
+  }
+
+  let formattedErrors = "";
+
+  for (const field in error.error) {
+    if (error.error.hasOwnProperty(field)) {
+      const fieldErrors: string[] = error.error[field];
+
+      fieldErrors.forEach((message) => {
+        const formattedField = field.replace(/_/g, ' ').replace(/\b\w/g, (l) => l.toUpperCase());
+        const errorMessage = `<p>${formattedField}: ${message}\n</p>`;
+        formattedErrors += errorMessage;
+      });
+    }
+  }
+
+  return formattedErrors;
 }
