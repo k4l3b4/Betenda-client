10621ea0c57ed6c651507c4fc08c8002d09e680c
CalebBlion <kaleba647@gmail.com>
2023-07-22 08:38:56 +0300

    Updated 2 requests

--- src/api/requests/contributions/poem/requests.ts
+++ src/api/requests/contributions/poem/requests.ts
@@ -1,14 +1,35 @@
 import axiosInstance from "@/api/axios-instance"
 import { CreatePoemType } from "@/types/contributions"
+import FormData from "form-data"
 
 
 export const registerPoem = async (values: CreatePoemType) => {
-    const response = await axiosInstance.post('contributions/poem', values)
+    const data = new FormData()
+    { values?.language ? data.append('language', values?.language) : undefined }
+    { values?.poem ? data.append('poem', values?.poem) : undefined }
+    { values?.recording ? data.append('media', values?.recording, values?.recording?.name) : undefined }
+    const response = await axiosInstance.post('contributions/poem', data, {
+        timeout: 90000,
+        headers: {
+            'Content-Type': values?.recording?.type
+        }
+    })
+    
     return response?.data
 }
 
 
 export const updatePoem = async ({ values, id }: { values: CreatePoemType, id: number }) => {
-    const response = await axiosInstance.patch(`contributions/poem?id=${id}`, values)
+    const data = new FormData()
+    { values?.language ? data.append('language', values?.language) : undefined }
+    { values?.poem ? data.append('poem', values?.poem) : undefined }
+    { values?.recording ? data.append('media', values?.recording, values?.recording?.name) : undefined }
+    const response = await axiosInstance.patch(`contributions/poem?id=${id}`, data, {
+        timeout: 90000,
+        headers: {
+            'Content-Type': values?.recording?.type
+        }
+    })
+
     return response?.data
 }
