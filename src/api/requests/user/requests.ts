6db3ae0ab2e83f8403e7fbd866266fa30420527e
CalebBlion <kaleba647@gmail.com>
2023-07-24 05:33:39 +0300

    Accept follow request api request

--- src/api/requests/user/requests.ts
+++ src/api/requests/user/requests.ts
@@ -2,7 +2,7 @@ import axiosInstance from "@/api/axios-instance"
 import { RegisterType } from "@/types/auth"
 
 export const getUser = async () => {
-    const response = await axiosInstance.get("/users/current_user")
+    const response = await axiosInstance.get("users/current_user")
     return response?.data
 }
 
@@ -26,8 +26,14 @@ export const unFollowUser = async (id: number) => {
     return response.data
 }
 
+
+export const acceptFollowRequest = async (id: number) => {
+    const response = await axiosInstance.post(`users/accept/${id}`)
+    return response.data
+}
+
 export const updateProfile = async (values: RegisterType) => {
-    const response = await axiosInstance.patch('users/get_user')
+    const response = await axiosInstance.patch('users/profile')
     return response.data
 }
 
