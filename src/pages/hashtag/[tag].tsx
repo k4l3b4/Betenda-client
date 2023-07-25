15e163a6dcd15c162a4d7db479e4107978e6b517
CalebBlion <kaleba647@gmail.com>
2023-07-25 23:03:48 +0300

    Minor updates

--- src/pages/hashtag/[tag].tsx
+++ src/pages/hashtag/[tag].tsx
@@ -1,9 +1,12 @@
 import { getPostsByTag } from "@/api/requests/post/requests";
+import FeedLayout from "@/components/layout/feed-layout";
 import Meta from "@/components/meta/meta";
 import PostsComp from "@/components/post/posts-comp";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getCookieValue } from "@/lib/utils";
 import { InfinitePostsType } from "@/types/post";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { QueryClient, dehydrate, useInfiniteQuery } from "@tanstack/react-query";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 
@@ -25,10 +28,10 @@ const ResourcesWithTag = () => {
 
 
     return (
-        <div>
-            <Meta title={`#${qParam}`} />
-            <div className="flex justify-center mt-2">
-                <Tabs defaultValue="word" className="w-full max-w-[750px]">
+        <FeedLayout>
+            <Meta title={`#${qParam}` ?? "Loading..."} />
+            <div className="w-full flex justify-center mt-2">
+                <Tabs defaultValue="post" className="w-full max-w-[650px]">
                     <TabsList className="flex flex-row flex-wrap justify-evenly items-center gap-2">
                         <TabsTrigger value="post">Posts</TabsTrigger>
                         <TabsTrigger value="article">Articles</TabsTrigger>
@@ -46,8 +49,25 @@ const ResourcesWithTag = () => {
                     </TabsContent>
                 </Tabs>
             </div>
-        </div>
+        </FeedLayout>
     );
 }
 
 export default ResourcesWithTag;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { req } = context;
+    const { tag } = context.params as ParsedUrlQuery
+
+
+    // Get the value of the 'sessionId' cookie
+    const sessionId = getCookieValue(req, 'sessionid')
+
+    const queryClient = new QueryClient()
+    await queryClient.prefetchInfiniteQuery({ queryKey: ['posts'], queryFn: () => getPostsByTag({ tag: tag as string, sessionId: sessionId }) })
+    return {
+        props: {
+            dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+        },
+    }
+}
