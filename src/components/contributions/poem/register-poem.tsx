6cda598b66ca99c72483ed495ca72865a5a6fe6e
CalebBlion <kaleba647@gmail.com>
2023-07-27 23:26:09 +0300

    Finished create functionality

--- src/components/contributions/poem/register-poem.tsx
+++ src/components/contributions/poem/register-poem.tsx
@@ -1,11 +1,17 @@
+import { registerPoem } from "@/api/requests/contributions/poem/requests";
+import SelectListbox from "@/components/select/select";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
-import { useForm } from "react-hook-form"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { useMutation } from "@tanstack/react-query";
+import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useToast } from "@/components/ui/use-toast";
+import { useContributionContext } from "@/context/contrib-context";
+import { DataType } from "@/types/global";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 const formSchema = z.object({
     poem: z.string().nonempty({ message: "The poem is required" }),
@@ -14,7 +20,18 @@ const formSchema = z.object({
 })
 
 const RegisterPoem = () => {
-    const { mutate, isLoading } = useMutation({})
+    const { toast } = useToast()
+    const { targetLabelValuePairs, isLoading, isError, refetch } = useContributionContext()
+    const [target, setTarget] = useState<DataType | null>(null)
+    const { mutate, isLoading: registering, isError: error } = useMutation({
+        mutationFn: registerPoem,
+        onSuccess: () => {
+            toast({
+                title: "Wo hoo",
+                description: "Amazing!, thank you for your contribution.",
+            })
+        }
+    })
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -25,22 +42,33 @@ const RegisterPoem = () => {
         },
     })
     function onSubmit(values: z.infer<typeof formSchema>) {
+        mutate(values)
     }
+
+
+    const handleTargetSelect = (value: DataType) => {
+        setTarget(value)
+        form.setValue('language', `${value?.value}`)
+    }
+
     return (
         <div className="w-full">
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="px-4 py-10 space-y-8 w-full rounded border">
-                    <h1 className="text-4xl font-semibold">Contribute a sentence!</h1>
+                    <h1 className="text-4xl font-semibold">Contribute a poem!</h1>
                     <FormField
                         control={form.control}
                         name="poem"
                         render={({ field }) => (
                             <FormItem className="w-full">
-                                <FormLabel>Saying</FormLabel>
+                                <FormLabel>Poem</FormLabel>
                                 <FormControl>
-                                    <Input type="text" placeholder="English" {...field} />
+                                    <Textarea className="max-h-[500px] bg-background" placeholder="Good morning" {...field} />
                                 </FormControl>
                                 <FormMessage />
+                                <FormDescription>
+                                    The language the poem is written in
+                                </FormDescription>
                             </FormItem>
                         )}
                     />
@@ -61,25 +89,24 @@ const RegisterPoem = () => {
                             </FormItem>
                         )}
                     />
+
                     <FormField
                         control={form.control}
                         name="language"
-                        render={({ field }) => (
+                        render={() => (
                             <FormItem className="w-full">
-                                <FormLabel>Poem</FormLabel>
+                                <FormLabel>Target Language</FormLabel>
                                 <FormControl>
-                                    <Textarea className="max-h-[500px] bg-background" placeholder="Good morning" {...field} />
+                                    <SelectListbox loading={isLoading} data={targetLabelValuePairs} placeholder="Select the language" selected={target} setSelected={(value) => handleTargetSelect(value)} />
                                 </FormControl>
                                 <FormMessage />
                                 <FormDescription>
-                                    The language the poem is written in
+                                    The target language
                                 </FormDescription>
                             </FormItem>
                         )}
                     />
-
-                    <Button type="submit">Submit</Button>
-                </form>
+                    <Button type="submit">{registering ? "Submitting..." : "Submit"}</Button>                </form>
             </Form>
         </div>
     );
