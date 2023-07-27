b3c54dd26f6ab8f0e16d74689c8aa93ec0a3b3e9
CalebBlion <kaleba647@gmail.com>
2023-07-27 23:24:40 +0300

    Style update

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -8,70 +8,63 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Bell, Plus } from "lucide-react"
 import { useUserContext } from "@/context/user-context"
+import Notifications from "./notifications/notifications"
 
-const ngClass = "no-underline hover:text-primary"
+const ngClass = "w-full no-underline hover:text-primary font-medium flex justify-start"
 
 export function SiteHeader() {
-  const { User } = useUserContext()
+  const { User, LoadingUser } = useUserContext()
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-foreground">
+    <header className="sticky top-0 z-40 w-full border-t-0 border-b bg-foreground">
       <div className="container flex h-16 items-center justify-between space-x-4 sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex select-none items-center space-x-1">
-            <Link href="/contributions" className={buttonVariants({ variant: "ghost" })}>
+          <nav className="flex select-none items-center space-x-2 justify-evenly">
+            <Link href="/contributions" className={buttonVariants({ variant: "ghost", className: "px-[10px]" })}>
               <Plus className="h-5 w-5" />
             </Link>
             <ThemeToggle />
-            <Button size="icon" variant={null}>
-              <Bell className="h-5 w-5" />
-            </Button>
-            <DropdownMenu>
-              <DropdownMenuTrigger className="w-fit" asChild>
-                <Button className="w-fit rounded-full" size="icon" variant={null}>
-                  <Avatar>
-                    <AvatarImage src={User?.profile_avatar} alt={`@${User?.user_name}`} />
-                    <AvatarFallback>{`${User?.first_name?.substr(0, 1)}${User?.last_name?.substr(0, 1)}`}</AvatarFallback>
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="mr-5 w-56">
-                <DropdownMenuLabel>
-                  <p>{`${User?.first_name} ${User?.last_name}` ?? ""}</p>
-                  <p className="text-muted-foreground text-sm">@{User?.first_name}</p>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem className="cursor-pointer">
-                    <Link className={ngClass} href="/profile">Profile</Link>
-                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-
-                  <DropdownMenuItem className="cursor-pointer">
-                    <Link className={ngClass} href="/profile/settings">
-                      Settings
-                    </Link>
-                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="cursor-pointer">
-                  <Link className={ngClass} href="/profile/settings">
-                    GitHub
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer">
-                  <Link className={ngClass} href="/profile/settings">
-                    Support
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  Log out
-                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <Notifications>
+              <Button className="w-fit px-[10px]" size="icon" variant="ghost">
+                <Bell className="h-5 w-5" />
+              </Button>
+            </Notifications>
+            {
+              LoadingUser ?
+                <div className="w-9 h-9 rounded-md bg-background animate-pulse border border-gray-600" />
+                :
+                User ?
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="w-fit" asChild>
+                      <Button className="w-9 h-9 rounded-md" size="icon" variant="ghost">
+                        <Avatar className="w-9 h-9">
+                          <AvatarImage className="w-full h-full object-cover" src={User?.profile_avatar} alt={`@${User?.user_name}`} />
+                          <AvatarFallback>{`${User?.first_name?.substr(0, 1)}${User?.last_name?.substr(0, 1)}`}</AvatarFallback>
+                        </Avatar>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="p-2 mr-5 w-72 flex flex-col items-start">
+                      <DropdownMenuLabel>
+                        <p>{`${User?.first_name} ${User?.last_name}` ?? ""}</p>
+                        <p className="text-muted-foreground text-xs">@{User?.user_name}</p>
+                      </DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <Link className={buttonVariants({ variant: "ghost", className: ngClass })} href="/profile"><p className="w-full text-start">Profile</p></Link>
+                      <Link className={buttonVariants({ variant: "ghost", className: ngClass })} href="/settings"><p className="w-full text-start">Settings</p></Link>
+                      <DropdownMenuSeparator />
+                      <Link target="_blank" className={buttonVariants({ variant: "ghost", className: ngClass })} href="/profile/settings"><p className="w-full text-start">GitHub</p></Link>
+                      <Link className={buttonVariants({ variant: "ghost", className: ngClass })} href="/donate"><p className="w-full text-start">Donate</p></Link>
+                      <Link className={buttonVariants({ variant: "ghost", className: ngClass })} href="/roadmap"><p className="w-full text-start">RoadMap</p></Link>
+                      <Link className={buttonVariants({ variant: "ghost", className: ngClass })} href="/profile/settings"><p className="w-full text-start">Support</p></Link>
+                      <DropdownMenuSeparator />
+                      <Button variant="ghost" className={`${ngClass} hover:bg-red-600 hover:text-white`}>Log out</Button>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  :
+                  <div className="w-9 h-9 rounded-md font-medium bg-background flex items-center justify-center border border-gray-600">
+                    <span>ክላ</span>
+                  </div>
+            }
           </nav>
         </div>
       </div>
