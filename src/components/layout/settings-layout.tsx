358ce7c469e97321ffec8ee69bdd7848a55b2f28
CalebBlion <kaleba647@gmail.com>
2023-07-27 23:41:15 +0300

    Custom Layout for settings

--- src/components/layout/settings-layout.tsx
+++ src/components/layout/settings-layout.tsx
@@ -5,30 +5,31 @@ import { buttonVariants } from '../ui/button';
 
 const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='h-[calc(100vh-64px)] w-screen p-2'>
-            <div className="flex flex-row h-full rounded-md bg-foreground p-3 gap-2">
-                <aside className='w-56'>
+        <div className='h-[calc(100vh-64px)] w-screen'>
+            <div className="flex flex-row h-full rounded-md p-2 gap-2">
+                <aside className='w-64 sticky top-0 bg-foreground rounded-md p-2'>
                     <h1 className="text-4xl font-extrabold">Settings</h1>
                     <div className='my-4'>
                         <Link href="/settings" className={`${buttonVariants({ variant: "ghost", className: "my-2 text-xl no-underline w-full font-bold" })}`}>
                             <p className="w-full text-start">Profile</p>
                         </Link>
-                        <Separator className="opacity-50" />
+                        <Separator className="opacity-60" />
                         <Link href="/settings/privacy" className={`${buttonVariants({ variant: "ghost", className: "my-2 text-xl no-underline w-full font-bold" })}`}>
                             <p className="w-full text-start">Privacy</p>
                         </Link>
-                        <Separator className="opacity-50" />
+                        <Separator className="opacity-60" />
                         <Link href="/settings/password" className={`${buttonVariants({ variant: "ghost", className: "my-2 text-xl no-underline w-full font-bold" })}`}>
                             <p className="w-full text-start">Password</p>
                         </Link>
-                        <Separator className="opacity-50" />
+                        <Separator className="opacity-60" />
                         <Link href="/settings/security" className={`${buttonVariants({ variant: "ghost", className: "my-2 text-xl no-underline w-full font-bold" })}`}>
                             <p className="w-full text-start">Security</p>
                         </Link>
                     </div>
                 </aside>
-                <Separator orientation='vertical' />
-                <main>{children}</main>
+                <main className="h-full bg-foreground w-full rounded-md p-2">
+                    {children}
+                </main>
             </div>
         </div>
     );
