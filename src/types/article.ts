fee69bec329321f169d8e865478ee3c6d0a6ffb7
CalebBlion <kaleba647@gmail.com>
2023-08-02 12:58:02 +0300

    Types for articles

---
+++ src/types/article.ts
@@ -0,0 +1,49 @@
+import { ReactionsType, UserType } from "@/types/global";
+
+export interface CreateArticleType {
+    title: string,
+    desc: string,
+    body: string,
+    image?: File,
+    authors?: string,
+    status: string,
+    featured: boolean,
+}
+
+export interface ArticleType {
+    id: number,
+    title: string,
+    slug: string,
+    desc: string,
+    body: string,
+    image?: string,
+    authors: UserType[],
+    status: string,
+    featured: boolean,
+    comments_count: string,
+    published_date: string,
+    modified_date: string,
+    reactions: ReactionsType,
+}
+
+export interface SimpleArticleType {
+    id: number,
+    title: string,
+    slug: string,
+    image: string,
+    status: string,
+}
+
+
+export interface PaginatedArticleType {
+    total_items: number,
+    page: number,
+    page_size: number,
+    results: ArticleType[]
+}[]
+
+
+export interface InfiniteArticlesType {
+    pageParams: Record<number, number>[],
+    pages: PaginatedArticleType[]
+}
