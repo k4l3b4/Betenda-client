0dc545d76fbdfe5e7db5285f050075127ad058eb
CalebBlion <kaleba647@gmail.com>
2023-08-02 12:53:19 +0300

    Added breakpoints

--- src/components/layout/feed-layout.tsx
+++ src/components/layout/feed-layout.tsx
@@ -1,7 +1,7 @@
 import Trending from "@/components/trending/trending";
 import Sidebar from "@/components/sidebar/sidebar";
 import { cn } from "@/lib/utils";
-import { useMediaQuery } from "usehooks-ts";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 type FeedLayoutType = {
     children: React.ReactNode,
@@ -13,6 +13,7 @@ type FeedLayoutType = {
 const FeedLayout: React.FC<FeedLayoutType> = ({ children, sideBar = "mount", trending = "mount", className }) => {
     const trendingMount = useMediaQuery('(min-width: 1097px)')
     const sidebarMount = useMediaQuery('(min-width: 851px)')
+
 
     return (
         <div className={cn("relative flex w-full flex-row justify-between px-2 gap-x-1 min-h-screen", className)}>
