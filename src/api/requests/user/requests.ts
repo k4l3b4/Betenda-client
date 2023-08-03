66b928a4b5d072d1eb4c5a4372a88bcd894bdacb
CalebBlion <kaleba647@gmail.com>
2023-08-03 21:47:50 +0300

    Created 4 api request functions

--- src/api/requests/user/requests.ts
+++ src/api/requests/user/requests.ts
@@ -1,13 +1,18 @@
 import axiosInstance from "@/api/axios-instance"
-import { RegisterType } from "@/types/auth"
+import { RegisterType, UpdateProfileType } from "@/types/auth"
+import FormData from "form-data"
 
 export const getUser = async () => {
     const response = await axiosInstance.get("users/current_user")
     return response?.data
 }
 
-export const getUserProfile = async ({ username }: { username: string }) => {
-    const response = await axiosInstance.get(`users/get_user?username=${username}`)
+export const getUserProfile = async ({ username, sessionId = "" }: { username: string, sessionId?: string }) => {
+    const response = await axiosInstance.get(`users/get_user?username=${username}`, {
+        headers: {
+            Cookie: `sessionid=${sessionId}`,
+        },
+    })
     return response.data
 }
 
@@ -27,15 +32,49 @@ export const unFollowUser = async (id: number) => {
 }
 
 
+export const getMutualFriends = async () => {
+    const response = await axiosInstance.get(`users/mutual_users`)
+    return response.data
+}
+
+export const getTopAccounts = async () => {
+    const response = await axiosInstance.get(`users/top_accounts`)
+    return response.data
+}
+
+export const getAuthors = async (search?: string) => {
+    const response = await axiosInstance.get(`users/search_author${search ? `?search=${search}` : ''}`)
+    return response.data
+}
+
 export const acceptFollowRequest = async (id: number) => {
     const response = await axiosInstance.post(`users/accept/${id}`)
     return response.data
 }
 
-export const updateProfile = async (values: RegisterType) => {
-    const response = await axiosInstance.patch('users/profile')
-    return response.data
-}
+export const updateProfile = async (values: UpdateProfileType) => {
+    const data = new FormData();
+    if (values?.first_name) data.append('first_name', values.first_name);
+    if (values?.last_name) data.append('last_name', values.last_name);
+    if (values?.user_name) data.append('user_name', values.user_name);
+    if (values?.bio) data.append('bio', values.bio);
+    if (values?.birth_date) data.append('birth_date', values.birth_date);
+    if (values?.email) data.append('email', values.email);
+    if (values?.profile_avatar) {
+        data.append('profile_avatar', values.profile_avatar, values.profile_avatar.name);
+    }
+    if (values?.profile_cover) {
+        data.append('profile_cover', values.profile_cover, values.profile_cover.name);
+    }
+    const response = await axiosInstance.patch('users/profile', data, {
+        timeout: 90000,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+
+    return response.data;
+};
 
 export const changePassword = async () => {
     const response = await axiosInstance.post('users/change_password')
