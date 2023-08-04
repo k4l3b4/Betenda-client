2c6af6155355707677c72a70049816854c88001a
CalebBlion <kaleba647@gmail.com>
2023-08-04 22:59:01 +0300

    Added optimistic updates when posts are created

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -6,12 +6,14 @@ import PostData from "@/components/post/post-data";
 import PostsComp from "@/components/post/posts-comp";
 import { getCookieValue } from "@/lib/utils";
 import { InfinitePostsType, PostType } from "@/types/post";
-import { QueryClient, dehydrate, useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import CircularProgress from "@mui/material/CircularProgress";
+import { QueryClient, dehydrate, useInfiniteQuery, useQuery, useQueryClient } from "@tanstack/react-query";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 
 const Post = () => {
+    const queryClient = useQueryClient()
     const router = useRouter()
     const { slug } = router.query as ParsedUrlQuery
     const cleanSlug = slug as string
@@ -22,28 +24,56 @@ const Post = () => {
     const repliesData = replies as InfinitePostsType
 
 
+    const onMutationSuccess = (data: any) => {
+        const currentData = queryClient.getQueryData<InfinitePostsType>(['get_replies', cleanSlug]);
+
+        if (currentData) {
+            console.log("current data:", currentData)
+            // Add the new notification to the first page
+            const updatedResults = [
+                data?.data, // getting this from the onSuccess handler in the useMutation hook
+                ...(currentData.pages[0]?.results || []),
+            ];
+            console.log("updated data:", updatedResults)
+            // Update the query data with the new notification added to the first page
+            queryClient.setQueryData<InfinitePostsType>(['get_replies', cleanSlug], {
+                pages: [
+                    {
+                        total_items: currentData.pages[0]?.total_items, // Preserve the total_items value
+                        page: currentData.pages[0]?.page, // Preserve the page value
+                        page_size: currentData.pages[0]?.page_size, // Preserve the page_size value
+                        results: updatedResults, // Update the property name to "results"
+                    },
+                    ...currentData.pages.slice(1),
+                ],
+                pageParams: currentData.pageParams,
+            });
+        }
+    }
+
+
     return (
         <FeedLayout>
             <Meta title={`${postData?.content?.substring(0, 15) ?? `${`${postData?.user?.first_name}'s post`}`}` ?? "Loading..."} />
-            < section className="rounded-md container flex flex-col items-center" id="feed">
+            <section className={`${loadingPosts ? "space-y-4" : ""} rounded-md container flex flex-col items-center mt-2 p-0`} id="feed">
                 {loadingPosts ?
-                    <div>
-                        Loading
+                    <div className="flex flex-col justify-center items-center mt-5">
+                        <CircularProgress color="inherit" thickness={4} size="2.3rem" />
                     </div>
                     :
                     <>
                         <PostData post={postData} />
-                        <CreatePost parent_id={postData?.id} placeholder={`Reply to ${postData?.user?.first_name}'s post`} />
+                        <CreatePost onSuccess={(data) => onMutationSuccess(data)} parent_id={postData?.id} placeholder={`Reply to ${postData?.user?.first_name}'s post`} />
                     </>
                 }
+                <h2 className="text-start w-full">Replies</h2>
                 {loadingReplies ?
-                    <div>
-                        Loading
+                    <div className="flex flex-col justify-center items-center mt-5">
+                        <CircularProgress color="inherit" thickness={4} size="2.3rem" />
                     </div>
                     :
                     <>
-                        <h2 className="text-start w-full">Replies</h2>
-                        <PostsComp postClassName="hover-anim" data={repliesData} error={repliesError} loading={loadingReplies} refetch={refetch} refetching={isRefetching} />
+                        <PostsComp classNames={{ post: "hover-anim" }} data={repliesData} error={repliesError} loading={loadingReplies} refetch={refetch} refetching={isRefetching} />
                     </>
                 }
             </section>
