bbf4d36b8081d64358a6b28aadca3765dac80b87
CalebBlion <kaleba647@gmail.com>
2023-08-09 23:55:27 +0300

    Added a readNotifications request function

--- src/api/requests/notification/requests.ts
+++ src/api/requests/notification/requests.ts
@@ -4,3 +4,8 @@ export const getNotifications = async ({ pageParam = 1 }: { pageParam?: number }
     const response = await axiosInstance.get(`notifications/list?page=${pageParam}`)
     return response?.data
 }
+
+export const readNotifications = async ({ ids }: { ids: string }) => {
+    const response = await axiosInstance.post('notifications/read', { 'ids': ids })
+    return response?.data
+}
