6aee9a9964609355cc6c475a1326907c9cc42a04
CalebBlion <kaleba647@gmail.com>
2023-08-12 01:12:09 +0300

    User posts component on profile page

---
+++ src/components/post/user-posts.tsx
@@ -0,0 +1,27 @@
+import { getUserPosts } from "@/api/requests/post/requests";
+import PostsComp from "@/components/post/posts-comp";
+import { InfinitePostsType } from "@/types/post";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import LoadMore from "../load-more/load-more";
+
+const UserPosts = ({ username }: { username: string }) => {
+    const { data, isError, isLoading, isFetchingNextPage, refetch, isRefetching, fetchNextPage, hasNextPage } = useInfiniteQuery(
+        {
+            queryKey: ['user_posts'],
+            queryFn: ({ pageParam }) => getUserPosts({ pageParam: pageParam, username: username }),
+            getNextPageParam: (lastPage) => lastPage?.page < lastPage?.pages_count ? lastPage?.page + 1 : undefined,
+            enabled: username ? true : false
+        })
+    const posts = data as InfinitePostsType
+
+    return (
+        <div className="h-full">
+            <PostsComp classNames={{ loading: "h-full", error: "h-full", post: { container: "px-2 py-4" } }} data={posts} loading={isLoading} error={isError} refetch={refetch} refetching={isRefetching} />
+            {hasNextPage &&
+                <LoadMore fetchNextPage={fetchNextPage} hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage} />
+            }
+        </div>
+    );
+}
+
+export default UserPosts;
