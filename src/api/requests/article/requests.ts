1be3a34716f1063b7ce134e7755f4733d8b0e49c
CalebBlion <kaleba647@gmail.com>
2023-08-19 23:56:46 +0300

    Added updateArticle api request

--- src/api/requests/article/requests.ts
+++ src/api/requests/article/requests.ts
@@ -1,5 +1,5 @@
 import axiosInstance from "@/api/axios-instance"
-import { CreateArticleType } from "@/types/article"
+import { CreateArticleType, UpdateArticleType } from "@/types/article"
 import FormData from "form-data"
 
 export const getArticles = async ({ pageParam = 1, sessionId = "" }: { pageParam?: number, sessionId?: string }) => {
@@ -13,7 +13,7 @@ export const getArticles = async ({ pageParam = 1, sessionId = "" }: { pageParam
     return response?.data
 }
 
-export const getArticleBySlug = async ({ slug, sessionId = "" }: { slug: string, sessionId?: string }) => {
+export const getArticleBySlug = async ({ slug, sessionId = null }: { slug: string, sessionId?: string | null }) => {
     const response = await axiosInstance.get(`articles/${slug}`, {
         headers: {
             Cookie: `sessionid=${sessionId}`,
@@ -59,4 +59,33 @@ export const createArticle = async ({ values }: { values: CreateArticleType }) =
     })
 
     return response.data
+}
+
+
+export const updateArticle = async ({ values, id }: { values: UpdateArticleType, id: number }) => {
+    const data = new FormData()
+    { values.title ? data.append('title', values.title) : null }
+    { values.desc ? data.append('desc', values.desc) : null }
+    { values.body ? data.append('body', values.body) : null }
+    { values.status ? data.append('status', values.status) : null }
+    { values.featured ? data.append('featured', values.featured) : null }
+    { values.authors ? data.append('authors', values?.authors) : null }
+    if (values?.image) {
+        data.append('image', values.image, values.image.name);
+    }
+
+    const response = await axiosInstance.patch(`articles/article?id=${id}`, data, {
+        timeout: 90000,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        }
+    })
+
+    return response.data
+}
+
+
+export const deleteArticleRequest = async ({ id }: { id: number }) => {
+    const response = await axiosInstance.delete(`articles/article?id=${id}`)
+    return response?.data
 }
