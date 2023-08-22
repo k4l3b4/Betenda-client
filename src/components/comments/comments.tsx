f3d52f05a35dc2bdcfc1ac203347485ee33064dd
CalebBlion <kaleba647@gmail.com>
2023-08-22 22:10:37 +0300

    UI update, comment input handling done

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -10,16 +10,27 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { AnimatePresence, motion } from "framer-motion";
 import { FrownIcon, GhostIcon } from "lucide-react";
-import { Fragment, useRef } from "react";
-import Comment from "./comment";
+import { Fragment, useEffect, useRef } from "react";
+import Comment from "@/components/comments/comment";
+import LoadMoreHider from "@/components/ui-utils/lead-more-hider";
+import LoadMore from "@/components/load-more/load-more";
 
-type CommentCompType = {
-    resource_type: string,
-    resource_id: number,
-}
 
-const Comments: React.FC<CommentCompType> = ({ resource_type, resource_id }) => {
-    const { commentInput, handleInputChange, handleReplyToComment, handleSaveComment, handleTopParent } = useReplyContext();
+const Comments = () => {
+    const { commentInput, handleInputChange, handleSaveComment, inputError, setInputError, resource_type, resource_id } = useReplyContext();
+
+    useEffect(() => {
+        if (inputError) {
+            const timeoutId = setTimeout(() => {
+                setInputError('');
+            }, 4000);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
+    }, [inputError]);
+
     const inputRef = useRef<HTMLInputElement>(null);
     const { User } = useUserContext()
     const {
@@ -33,16 +44,36 @@ const Comments: React.FC<CommentCompType> = ({ resource_type, resource_id }) => 
         hasNextPage
     } = useInfiniteQuery(
         {
-            queryKey: ['comments'],
+            queryKey: ['comments', resource_id],
             queryFn: ({ pageParam }) => getComments({ pageParam: pageParam, resource_id: resource_id, resource_type: resource_type }),
             getNextPageParam: (lastPage) => lastPage?.data?.page < lastPage?.data?.pages_count ? lastPage?.data?.page + 1 : undefined,
         }
     )
 
+
     const comments = data as InfiniteCommentsType
 
     return (
         <>
+            <section className="my-2 flex items-center gap-x-2" id="comment or reply">
+                <Avatar className="w-12 h-12 mt-1">
+                    <AvatarImage src={User?.profile_avatar} alt={`@${User?.user_name}`} />
+                    <AvatarFallback>{User?.first_name?.substring(0, 1) + User?.last_name?.substring(0, 1)}</AvatarFallback>
+                </Avatar>
+                <section id="comment_input" className="relative w-full">
+                    <div className="relative w-full flex items-center justify-between gap-x-2">
+                        <Input
+                            ref={inputRef}
+                            value={commentInput}
+                            onChange={handleInputChange}
+                            className="w-full bg-foreground font-medium py-4"
+                            placeholder="What do you think?" type="text"
+                        />
+                        <Button onClick={handleSaveComment} variant="ghost">Post</Button>
+                    </div>
+                    {inputError && <p className="absolute -bottom-4 text-xs text-red-500 left-0">{inputError}</p>}
+                </section>
+            </section>
             {
                 isLoading ?
                     <div className="flex flex-col justify-center items-center mt-5">
@@ -67,22 +98,6 @@ const Comments: React.FC<CommentCompType> = ({ resource_type, resource_id }) => 
                             :
                             <section id="comments" className="w-full space-y-6">
                                 <h4>{`${comments?.pages?.[0]?.total_items} Comment(s)`}</h4>
-                                <div className="my-2 flex items-center gap-x-2">
-                                    <Avatar className="w-12 h-12 mt-1">
-                                        <AvatarImage src={User?.profile_avatar} alt={`@${User?.user_name}`} />
-                                        <AvatarFallback>{User?.first_name?.substring(0, 1) + User?.last_name?.substring(0, 1)}</AvatarFallback>
-                                    </Avatar>
-                                    <section id="comment_input" className="w-full flex items-center justify-between gap-x-2">
-                                        <Input
-                                            ref={inputRef}
-                                            value={commentInput}
-                                            onChange={handleInputChange}
-                                            className="w-full bg-foreground font-medium py-4"
-                                            placeholder="What do you think?" type="text"
-                                        />
-                                        <Button onClick={handleSaveComment} variant="ghost">Post</Button>
-                                    </section>
-                                </div>
                                 <div className="space-y-4">
                                     <AnimatePresence mode={"popLayout"}>
                                         {comments?.pages?.map(page =>
@@ -92,7 +107,6 @@ const Comments: React.FC<CommentCompType> = ({ resource_type, resource_id }) => 
                                                         <motion.div
                                                             key={comment?.id}
                                                             layout
-
                                                             animate={{ scale: 1, opacity: 1 }}
                                                             exit={{ scale: 0.9, opacity: 0 }}
                                                             transition={{ type: "spring", duration: 0.5 }}
@@ -104,6 +118,9 @@ const Comments: React.FC<CommentCompType> = ({ resource_type, resource_id }) => 
                                             }))}
                                     </AnimatePresence>
                                 </div>
+                                <LoadMoreHider page={comments?.pages?.[1] ? true : false} loading={isFetchingNextPage}>
+                                    <LoadMore fetchNextPage={fetchNextPage} hasNextPage={hasNextPage as boolean} isFetchingNextPage={isFetchingNextPage} />
+                                </LoadMoreHider>
                             </section>
             }
         </>
