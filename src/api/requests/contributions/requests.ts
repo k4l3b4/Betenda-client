d97c285a02f92384ccc3d9adbfceb0cb0606b4fb
CalebBlion <kaleba647@gmail.com>
2023-08-22 22:05:14 +0300

    Added register languages api request function

--- src/api/requests/contributions/requests.ts
+++ src/api/requests/contributions/requests.ts
@@ -1,6 +1,12 @@
 import axiosInstance from "@/api/axios-instance"
+import { RegisterLanguageType } from "@/types/contributions"
 
 export const getLanguages = async () => {
     const response = await axiosInstance.get('contributions/langs')
+    return response?.data
+}
+
+export const registerLanguage = async (values: RegisterLanguageType) => {
+    const response = await axiosInstance.post('/contributions/langs', values)
     return response?.data
 }
